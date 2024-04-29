2d8c9a94a072b6431c0d3f969b5990740d1902d0
piyushgpt <piyushthegreat1@gmail.com>
2024-04-29 15:42:41 +0530

    changes

--- my-app/server/controllers/CVUpload.js
+++
@@ -1,7 +0,0 @@
-exports. upload=async(req,res)=>{
-    console.log(req.body);
-    return res.status(200).json({
-        success:true,
-        message:"ok"
-    })
-}
--- my-app/server/routes/cvUpload.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const { upload } = require("../controllers/CVUpload")
-
-router.post("/upload", upload)
-
-module.exports = router
